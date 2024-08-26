dd59952d94207baba0dbeb306850ba092d3908e6
SiphoChris <siphonkebe@gmail.com>
2024-08-26 14:58:58 +0200

    project cleanup, and basic setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,13 @@
 import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+import state from './state'
+import actions from './actions'
+import mutations from './mutations'
+
+const store = createStore({
+  state,
+  mutations,
+  actions
 })
+
+export default store
