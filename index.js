780c0b897ac10e5bdc52ada0762b5b719ece3a11
SiphoChris <siphonkebe@gmail.com>
2024-08-26 15:58:09 +0200

    project structure

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import express from "express";
 import cors from "cors";
+import corsResolver from "./middlewares/CORSResolver";
+import globalErrorHandler from "./middlewares/GlobalErrorHandling";
 import "dotenv/config";
 import jwt from "jsonwebtoken";
 import mysql from "mysql2/promise";
 import nodemailer from "nodemailer";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+
+
+const server = express();
+
 
 // global variables
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+// middlewares
+server.use(express.json());
+server.use(corsResolver);
+server.use(cors());
+server.use(express.static(join(__dirname, "static")));
+server.use(express.urlencoded({ extended: true }));
